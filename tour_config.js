6e5f03886f0d07af2dbddd712d27614d5ada5326
Charles L. Wallace <charliewallace@gmail.com>
2026-03-29 21:02:57 -0700

    Genericized1:  extracted tour_config.js

---
+++ tour_config.js
@@ -0,0 +1,45 @@
+var TourConfig = {
+    PageTitle: "Highland Ave Tour",
+    MetaDescription: "Tour of Helen and Bim Wallace's Highland Ave house, 1999.",
+    HomeIndex: 231,
+    DefaultDescrip: "Wallace Tour",
+    DefaultVerboseDescrip: "Highland Ave, Solana Beach, CA",
+    InfoHtml: `
+      <div style="font-size:29px; font-style:italic; font-weight:bold; text-indent: 15px; line-height: 2.5;">
+      Tour of Helen and Bim Wallace's House
+      </div>
+      <div style="line-height:1.1;">
+        This is a tour of the home of Helen and Bim Wallace, circa October 1999, 
+        located at 1036 Highland Drive, Solana Beach, California. They lived here from late 1969 to May 2015.
+        <p>Arrow buttons are provided allowing you to navigate the house and grounds. 
+        Most of the photos were taken on that day, but a lot of family pictures from both earlier 
+        and later are also included; you can click on "Later" and "Earlier" buttons to see them. 
+        TIP: On a laptop or PC, you can use the keyboard arrow keys to move around, 
+        O/C keys to open/close, N/P for next/previous, and so on.
+      </div>
+    `,
+    ShortcutsHtml: `
+      <p style="padding-left:30px;">Click on the shortcut links below to jump directly to places in the tour:</p>
+      <ul class="shortcut-links">
+        <li><a href="#" onclick="doJumpToIndex(960)">Helen Portraits</a> - Good portraits of Helen. <a href="#" onclick="doJumpToIndex(935)">Helen with others</a>.</li>
+        <li><a href="#" onclick="doJumpToIndex(949)">Bim Portraits</a> - Good portraits of Bim. <a href="#" onclick="doJumpToIndex(917)">Bim with others</a>.</li>
+        <li><a href="#" onclick="doJumpToIndex(954)">Helen and Bim</a>.</li>
+        <li><a href="#" onclick="doJumpToIndex(822)">Hogmenay 2008</a> - Family Hogmanay (Christmas) celebration on Jan 1st, 2008.</li>
+        <li><a href="#" onclick="doJumpToIndex(827)">65th Anniversary & Helen's 90th</a> - Celebration on Apr 12, 2012, of their 65th anniv. (Apr 6th) and Helen's 90th birthday (Apr 10).</li>
+        <li><a href="#" onclick="doJumpToIndex(908)">Reunions</a> - Family Reunions & groups, in 1973, May 77, Apr 96, Aug 87, July 01, and July 09.</li>
+        <li><a href="#" onclick="doJumpToIndex(540)">Bim machine demos</a> - Bim in the middle shop doing demos of the Tape Caster, Printer, and Stacker he was building.</li>
+        <li><a href="#" onclick="doJumpToIndex(148)">Kitchen</a> - Shortcut to the kitchen.</li>
+        <li><a href="#" onclick="doJumpToIndex(502)">Machine Shop</a> - Shortcut to Bim in the Machine Shop.</li>
+        <li><a href="#" onclick="doJumpToIndex(375)">Boat</a> - Shortcut to Bim's sailboat, the Gertrude. You can go inside.</li>
+        <li><a href="#" onclick="doJumpToIndex(349)">Outside Lab</a> - Shortcut to just outside the basement lab door.</li>
+      </ul>
+      <p><i>Photos by Charles L. Wallace.</i></p>
+    `,
+    ZipMacros: {
+      957: "bllflflflloffflffofrrfrfro",
+      520: "rflflofffffofrrfrfrfle",
+      234: "frfroffffrofrflfffrffrfll",
+      232: "ffffffrrofrfffofrf",
+      666: "rfrffrflflloffflffofrrff"
+    }
+};
